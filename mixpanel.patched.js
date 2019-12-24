3c161fb714fd6bab1c21b9f3aea48c5f2e0a0f43
Eugene Chechuryn <e.chechurin@gmail.com>
2019-12-24 15:17:44 +0200

    Remove cookies/localStorage

--- mixpanel.patched.js
+++ mixpanel.patched.js
@@ -964,150 +964,51 @@ _.getHashParam = function(hash, param) {
 // _.cookie
 // Methods partially borrowed from quirksmode.org/js/cookies.html
 _.cookie = {
-    get: function(name) {
-        var nameEQ = name + '=';
-        var ca = document$1.cookie.split(';');
-        for (var i = 0; i < ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) == ' ') {
-                c = c.substring(1, c.length);
-            }
-            if (c.indexOf(nameEQ) === 0) {
-                return decodeURIComponent(c.substring(nameEQ.length, c.length));
-            }
-        }
-        return null;
+    get: function (name) {
+        return
     },
 
-    parse: function(name) {
-        var cookie;
-        try {
-            cookie = _.JSONDecode(_.cookie.get(name)) || {};
-        } catch (err) {
-            // noop
-        }
-        return cookie;
+    parse: function (name) {
+        return
     },
 
     set_seconds: function(name, value, seconds, cross_subdomain, is_secure) {
-        var cdomain = '',
-            expires = '',
-            secure = '';
-
-        if (cross_subdomain) {
-            var matches = document$1.location.hostname.match(DOMAIN_MATCH_REGEX),
-                domain = matches ? matches[0] : '';
-
-            cdomain = ((domain) ? '; domain=.' + domain : '');
-        }
-
-        if (seconds) {
-            var date = new Date();
-            date.setTime(date.getTime() + (seconds * 1000));
-            expires = '; expires=' + date.toGMTString();
-        }
-
-        if (is_secure) {
-            secure = '; secure';
-        }
-
-        document$1.cookie = name + '=' + encodeURIComponent(value) + expires + '; path=/' + cdomain + secure;
+        return
     },
 
-    set: function(name, value, days, cross_subdomain, is_secure) {
-        var cdomain = '', expires = '', secure = '';
-
-        if (cross_subdomain) {
-            var matches = document$1.location.hostname.match(DOMAIN_MATCH_REGEX),
-                domain = matches ? matches[0] : '';
-
-            cdomain   = ((domain) ? '; domain=.' + domain : '');
-        }
-
-        if (days) {
-            var date = new Date();
-            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-            expires = '; expires=' + date.toGMTString();
-        }
-
-        if (is_secure) {
-            secure = '; secure';
-        }
-
-        var new_cookie_val = name + '=' + encodeURIComponent(value) + expires + '; path=/' + cdomain + secure;
-        document$1.cookie = new_cookie_val;
-        return new_cookie_val;
+    set: function (name, value, days, cross_subdomain, is_secure) {
+        return
     },
 
     remove: function(name, cross_subdomain) {
-        _.cookie.set(name, '', -1, cross_subdomain);
+        return
     }
 };
 
 // _.localStorage
-var _localStorage_supported = null;
 _.localStorage = {
-    is_supported: function() {
-        if (_localStorage_supported !== null) {
-            return _localStorage_supported;
-        }
-
-        var supported = true;
-        try {
-            var key = '__mplssupport__',
-                val = 'xyz';
-            _.localStorage.set(key, val);
-            if (_.localStorage.get(key) !== val) {
-                supported = false;
-            }
-            _.localStorage.remove(key);
-        } catch (err) {
-            supported = false;
-        }
-        if (!supported) {
-            console$1.error('localStorage unsupported; falling back to cookie store');
-        }
-
-        _localStorage_supported = supported;
-        return supported;
+    is_supported: function () {
+        return false
     },
 
     error: function(msg) {
-        console$1.error('localStorage error: ' + msg);
+        return
     },
 
     get: function(name) {
-        try {
-            return window.localStorage.getItem(name);
-        } catch (err) {
-            _.localStorage.error(err);
-        }
         return null;
     },
 
     parse: function(name) {
-        try {
-            return _.JSONDecode(_.localStorage.get(name)) || {};
-        } catch (err) {
-            // noop
-        }
         return null;
     },
 
-    set: function(name, value) {
-        try {
-            window.localStorage.setItem(name, value);
-        } catch (err) {
-            _.localStorage.error(err);
-        }
+    set: function (name, value) {
+        return
     },
 
     remove: function(name) {
-        try {
-            window.localStorage.removeItem(name);
-        } catch (err) {
-            _.localStorage.error(err);
-        }
+        return
     }
 };
 
